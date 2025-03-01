287757457dbfd260b236df108377c1e29a215666
Abijith B <abijith.b@udaan.com>
2025-03-01 19:31:50 +0530

    change asset format

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/irctc-ticket/',
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
+  },
   plugins: [react(), VitePWA({
     registerType: 'prompt',
     injectRegister: false,
